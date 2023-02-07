b2fda9417d3309e072c37965955d77c9fa111ebc
raevaaM <maria.raeva04@gmail.com>
2023-02-07 22:47:14 +0200

    changes

---
+++ Adventures/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Adventures.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Adventures/wwwroot/js/site.js
+++ Adventures/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//var nav = document.querySelector('nav');
+
+//window.addEventListener('scroll', function () {
+//    if (this.window.pageYOffset > 100) {
+//        nav.classList.add('bg-dark', 'shadow');
+//    }
+//    else {
+//        nav.classList.remove('bg-dark', 'shadow');
+//    }
+//});
