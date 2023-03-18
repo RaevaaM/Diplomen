eb5e1c3aa3dd374600ed3f5b9b6e6a390aa39b2f
raevaaM <maria.raeva04@gmail.com>
2023-03-18 19:23:11 +0200

    new

---
+++ Adventures/Data/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace Adventures.Data
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longtitude { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ Adventures/Data/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Adventures.Data
+{
+    public class User:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid RoleId { get; set; }
+
+    }
+}
